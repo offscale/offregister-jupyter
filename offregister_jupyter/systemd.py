0b83577baf28ca5ee017377d67d5b1791596bf95
Samuel Marks <samuelmarks@gmail.com>
2017-07-30 14:31:50 +1000

    Init

---
+++ offregister_jupyter/systemd.py
@@ -0,0 +1,23 @@
+from offregister_fab_utils.ubuntu.systemd import restart_systemd, install_upgrade_service
+
+
+def install_jupyter_notebook_server(listen_ip, notebook_dir, pythonpath,
+                                    listen_port='8888', conf_name='jupyter_notebook',
+                                    extra_opts=None, **kwargs):
+    install_upgrade_service(
+        conf_name,
+        conf_local_filepath=kwargs.get('systemd-conf-file'),
+        context={
+            'ExecStart': ' '.join(
+                ('{pythonpath}/bin/jupyter notebook'.format(pythonpath=pythonpath),
+                 "--NotebookApp.notebook_dir='{notebook_dir}'".format(notebook_dir=notebook_dir),
+                 '--NotebookApp.ip={listen_ip}'.format(listen_ip=listen_ip),
+                 '--NotebookApp.port={listen_port}'.format(listen_port=listen_port),
+                 '--Session.username={User}'.format(User=kwargs['User']),
+                 extra_opts if extra_opts else '')),
+            'Environments': kwargs['Environments'],
+            'WorkingDirectory': pythonpath,
+            'User': kwargs['User'], 'Group': kwargs['Group']
+        }
+    )
+    return restart_systemd(conf_name)
