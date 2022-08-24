8f329d65a19156b2416ef3bb4c47670997924ba1
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-23 22:13:39 -0400

    [*] Upgrade to latest threadsafe Python 3 compatible Fabric

--- offregister_jupyter/ubuntu.py
+++ offregister_jupyter/ubuntu.py
@@ -1,39 +1,37 @@
 from os import environ
 
 import offregister_nginx_static.ubuntu as nginx
-from fabric.context_managers import shell_env
 from fabric.contrib.files import exists
-from fabric.operations import run, sudo
 from offregister_fab_utils.apt import apt_depends
 
 from offregister_jupyter.systemd import install_jupyter_notebook_server
 
 
 def install_jupyter_notebook0(virtual_env=None, *args, **kwargs):
-    home = kwargs.get("HOMEDIR", run("echo $HOME", quiet=True))
+    home = kwargs.get("HOMEDIR", c.run("echo $HOME", hide=True).stdout.rstrip())
     virtual_env = virtual_env or "{home}/venvs/jupyter".format(home=home)
 
-    if not exists(virtual_env):
-        apt_depends("python3-pip", "python3-venv")
-        sudo("pip3 install -U pip wheel setuptools")
-        run("mkdir -p {}".format(virtual_env))
-        run("python3 -m venv {}".format(virtual_env))
+    if not exists(c, runner=c.run, path=virtual_env):
+        apt_depends(c, "python3-pip", "python3-venv")
+        c.sudo("pip3 install -U pip wheel setuptools")
+        c.run("mkdir -p {}".format(virtual_env))
+        c.run("python3 -m venv {}".format(virtual_env))
 
-    with shell_env(
+    env = dict(
         VIRTUAL_ENV=virtual_env,
         PYTHONPATH=virtual_env,
         PATH="{virtual_env}/bin:$PATH".format(virtual_env=virtual_env),
-    ):
-        run("pip3 install -U pip wheel setuptools")
-        run("pip3 install -U jupyter")
+    )
+    c.run("pip3 install -U pip wheel setuptools", env=env)
+    c.run("pip3 install -U jupyter", env=env)
 
     user, group = (lambda ug: (ug[0], ug[1]) if len(ug) > 1 else (ug[0], ug[0]))(
-        run(
-            '''printf '%s\t%s' "$USER" "$GROUP"''', quiet=True, shell_escape=False
+        c.run(
+            '''printf '%s\t%s' "$USER" "$GROUP"''', hide=True, shell_escape=False
         ).split("\t")
     )
     notebook_dir = kwargs.get("notebook_dir", "{home}/notebooks".format(home=home))
-    run("mkdir -p '{notebook_dir}'".format(notebook_dir=notebook_dir))
+    c.run("mkdir -p '{notebook_dir}'".format(notebook_dir=notebook_dir))
 
     return install_jupyter_notebook_server(
         pythonpath=virtual_env,
@@ -60,7 +58,7 @@ def install_jupyter_notebook0(virtual_env=None, *args, **kwargs):
 
 def setup_nginx1(domain, *args, **kwargs):
     sites_enabled = kwargs.get("sites-enabled", "/etc/nginx/sites-enabled")
-    return nginx.setup_conf0(
+    return nginx.setup_custom_conf2(
         nginx_conf="proxy-pass.conf",
         SERVER_NAME=domain,
         SERVER_LOCATION="127.0.0.1:{}".format(kwargs.get("listen_port", "8888")),
